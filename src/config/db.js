55148d32b394e6a6f382ed6576fbb40efaf09afe
Jessica <jessica7rm@gmail.com>
2023-08-14 15:39:17 +0200

    cambios

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const { connect } = require('mongoose');
 
-const URL = "mongodb+srv://jessica7rm:Jessica2023@cluster0.kcjyrbp.mongodb.net/?retryWrites=true&w=majority"
+const URL = process.env.MONGO;
 
 connect(URL)
     .then(() => {
